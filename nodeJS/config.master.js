a5204f88d2691952db703268cd03ccd287d1b8d1
Andrea R. Nigro <andrea.nigro@ogilvy.com>
2018-03-03 18:11:55 +0100

    -

---
+++ nodeJS/config.master.js
@@ -0,0 +1,44 @@
+module.exports = {
+
+	MYSQL : {
+		"host"     : "localhost",
+		"user"     : "root",
+		"password" : "example",
+		"database" : "site-images-optimization"
+	},
+
+	REDIS : {
+		"host" : "127.0.0.1",
+		"port" : "6379"
+	},
+
+	GOOGLE_PSI_KEY : "AIzaSyCLRB-SnqDh3hEXAYdDvlQVi0iURBvHR1g",
+	//GOOGLE_PSI_KEY : "AIzaSyALHl_PAlecsrHffP_MUpI8-WRmPfNPaqA",
+
+	temp_storage : 'tmp/storage',
+
+	domain_filter : [
+		'www.galbani.it'
+	],
+
+	QUEUE : {
+
+		step1 : 'pages',
+
+		step2 : 'pagesToOptimize'
+
+	},
+
+	ARENA : {
+		queues : [
+			{
+				name     : 'pages',
+				"hostId" : "local",
+			},
+			{
+				name     : 'pagesToOptimize',
+				"hostId" : "local",
+			},
+		]
+	}
+};
